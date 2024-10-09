bd58975788b974dbc63dc2eb173f7f1f1f305968
MarcusSarmento <mvddssarmento@hotmail.com>
2024-10-09 00:55:35 -0300

    Concluido

---
+++ Dia_32/react100/components/Mycomponents.jsx
@@ -0,0 +1,10 @@
+function Mycomponents(){
+    return(
+        <div>
+            <h1>Hello World</h1>
+            <p>este é meu primeiro componente</p>
+        </div>
+    )
+}
+
+export default Mycomponents;
