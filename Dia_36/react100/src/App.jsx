492765336f40cac5e40089194c511e982583daf4
MarcusSarmento <mvddssarmento@hotmail.com>
2024-10-11 00:27:18 -0300

    Concluido

---
+++ Dia_36/react100/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import Mycomponents from '../../../Dia_33/react100/components/Mycomponents'
+import CondicionalComponent from '../../../Dia_34/react100/components/CondicionalComponent'
+import Counter from '../components/Contador'
+import GithubUser from '../components/GithubUser'
+
+function App() {
+  
+
+  return (
+   
+   <div>
+      <GithubUser userName="MarcusSarmento"/> 
+    </div>
+   
+  )
+}
+
+export default App
