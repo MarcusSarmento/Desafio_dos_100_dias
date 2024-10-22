ca9922520526ea72304e1bed71b9e576b550e64e
MarcusSarmento <mvddssarmento@hotmail.com>
2024-10-21 23:22:34 -0300

    Concluido

--- Dia_43/serve.js
+++ Dia_43/serve.js
@@ -1,9 +1,23 @@
-const express = requier('express')
+const express = require('express')
 
-const cors = requier('cors')
+const cors = require('cors')
 
 const app = express()
 app.use (express.json())
 app.use (cors()) 
+
+
+// rota com http get
+
+//app.get('/users',(request, response) => {
+//    return response.send('Lista de usuários')
+//})
+
+// rota com parametros
+
+app.get('/users/:id',(request, response) => {
+    const userId = request.params.id;
+    return response.send(`User ID: ${userId}`)
+})
 
 app.listen(8080, () => console.log('Servidor conectado na porta 8080'))
