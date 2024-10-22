98a222d5c97c131b0e214ecb36a4af9bac84ddad
MarcusSarmento <mvddssarmento@hotmail.com>
2024-10-21 22:45:25 -0300

    Revert "Concluido"
    This reverts commit 916f80924763442ecd507e0f91b41b8a19efed94.

--- Dia_42/serve.js
+++
@@ -1,9 +0,0 @@
-const express = requier('express')
-
-const cors = requier('cors')
-
-const app = express()
-app.use (express.json())
-app.use (cors()) 
-
-app.listen(8080, () => console.log('Servidor conectado na porta 8080'))
