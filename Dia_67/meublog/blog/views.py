28d84e679867acfaff5b8facafdf291955fa55ab
MarcusSarmento <mvddssarmento@hotmail.com>
2024-11-11 02:38:07 -0300

    Concluido

---
+++ Dia_67/meublog/blog/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post
+from .forms import PostForm
+
+def home(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/home.html', {'posts': posts})
+
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id) 
+    return render (request,'blog/post_detail.html', {'post': post})
+
+def new_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            return redirect('home')
+        else: 
+            form = PostForm()
+            return render(request, 'blog/new_post.html', {'form':form})
