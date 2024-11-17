78f77c02f02ac6a3492537865806258f3411d38d
MarcusSarmento <mvddssarmento@hotmail.com>
2024-11-17 00:13:58 -0300

    Concluido

--- Dia_53/main.py
+++ Dia_53/main.py
@@ -4,9 +4,10 @@ principal = float(input("Digite o valor principal: "))
 taxa_juros = float(input("Digite a taxa de juros anuals (%): "))
 anos = int(input("Digite a quantidade de anos: "))
 
+# Função que calcula o juros
 def calcula_juros_composto(principal, taxa_juros, anos):
     montante = principal * (1+(taxa_juros/100)) ** anos
     return montante
 
 montante = calcula_juros_composto(principal,taxa_juros,anos)
-print(f"O montante após {anos} anos será: {montante}100")
+print(f"O montante após {anos} anos será: {montante:.2f}")
