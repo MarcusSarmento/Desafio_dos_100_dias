a82dc04b1ec15fa064d055e180ba94f05cf3bad6
MarcusSarmento <mvddssarmento@hotmail.com>
2024-12-03 22:09:41 -0300

    Concluido

---
+++ Dia_89/main.py
@@ -0,0 +1,40 @@
+#Projeto Lista de Tarefas Flask - Parte 3
+
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__, template_folder="templates")
+
+tarefas = [{"tarefas":"Tarefa exemplo", "oncluido": False}]
+
+@app.route("/")
+def index():
+    return render_template("index.html", tarefas = tarefas)
+
+@app.route("/add", methods=["POST"])
+def add():
+    tarefa = request.form['tarefa']
+    tarefas.append({"tarefas":tarefa, "concluido":False})
+    return redirect (url_for("index"))
+
+@app.route("/edit/<int:index>", methods=["GET", "POST"])
+def edit(index):
+    tarefa = tarefas[index]
+    if request.method == "POST":
+        tarefa['tarefa'] = request.form['tarefa']
+        return redirect(url_for("index"))
+    else:
+        return render_template("edit.html", tarefa=tarefa, index=index)
+
+@app.route('/check/<int:index>')
+def check(index):
+    tarefas[index]['concluido'] = not tarefas[index]['concluido']
+    return redirect(url_for("index"))
+
+@app.route('/delete/<int:index>')
+def delete(index):
+    del tarefas[index]
+    return redirect(url_for("index"))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
